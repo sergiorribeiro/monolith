c371d572ac2efccc4daa1124ef78ca22832d6a21
Sérgio Ribeiro <sergio.ribeiro.comon@gmail.com>
2018-09-17 23:44:59 +0100

    God damn it!

--- foundations/monolith.js
+++ foundations/monolith.js
@@ -9,7 +9,7 @@ function monolith() {
     var queuedTransition = "";
 
     self.consts = {
-        version: "0.3a",
+        version: "0.4a",
         stage_transitions: {
             CROSSFADE: 1,
             SLIDE_LEFT: 2,
